de2f4cbf4f237ec18c4fa62de214352386b0f0a4
Yoshika Tsurudome <yotsurud@student.42.fr>
2025-04-12 16:36:04 +0900

    fix: cal fix to Norm

--- src/calc/multi_divid_vector.c
+++ src/calc/multi_divid_vector.c
@@ -6,48 +6,48 @@
 /*   By: yotsurud <yotsurud@student.42.fr>          #+#  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025-04-12 05:28:37 by yotsurud          #+#    #+#             */
-/*   Updated: 2025-04-12 05:28:37 by yotsurud         ###   ########.fr       */
+/*   Updated: 2025/04/12 16:32:37 by yotsurud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "calc.h"
 
-t_xyz   multi_v1_v2(t_xyz v1, t_xyz v2)
+t_xyz	multi_v1_v2(t_xyz v1, t_xyz v2)
 {
-    t_xyz   tmp;
+	t_xyz	tmp;
 
-    tmp.x = v1.x * v2.x;
-    tmp.y = v1.y * v2.y;
-    tmp.z = v1.z * v2.z;
-    return (tmp);
+	tmp.x = v1.x * v2.x;
+	tmp.y = v1.y * v2.y;
+	tmp.z = v1.z * v2.z;
+	return (tmp);
 }
 
-t_xyz   multi_v_f(t_xyz v, double f)
+t_xyz	multi_v_f(t_xyz v, double f)
 {
-    t_xyz   tmp;
+	t_xyz	tmp;
 
-    tmp.x = v.x * f;
-    tmp.y = v.y * f;
-    tmp.z = v.z * f;
-    return (tmp);
+	tmp.x = v.x * f;
+	tmp.y = v.y * f;
+	tmp.z = v.z * f;
+	return (tmp);
 }
 
-t_xyz   divid_v1_v2(t_xyz v1, t_xyz v2)
+t_xyz	divid_v1_v2(t_xyz v1, t_xyz v2)
 {
-    t_xyz   tmp;
+	t_xyz	tmp;
 
-    tmp.x = v1.x / v2.x;
-    tmp.y = v1.y / v2.y;
-    tmp.z = v1.z / v2.z;
-    return (tmp);
+	tmp.x = v1.x / v2.x;
+	tmp.y = v1.y / v2.y;
+	tmp.z = v1.z / v2.z;
+	return (tmp);
 }
 
-t_xyz   divid_v_f(t_xyz v, double f)
+t_xyz	divid_v_f(t_xyz v, double f)
 {
-    t_xyz   tmp;
+	t_xyz	tmp;
 
-    tmp.x = v.x / f;
-    tmp.y = v.y / f;
-    tmp.z = v.z / f;
-    return (tmp);
+	tmp.x = v.x / f;
+	tmp.y = v.y / f;
+	tmp.z = v.z / f;
+	return (tmp);
 }
